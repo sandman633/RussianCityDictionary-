a9ffc39394aa2e87622770bec07d509f921a2fe8
sandman633 <69395587+sandman633@users.noreply.github.com>
2020-12-03 21:06:53 +0300

    Добавьте файлы проекта.

---
+++ RussianCityDictionary/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace RussianCityDictionary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.russianscitydbDataSet = new RussianCityDictionary.russianscitydbDataSet();
+            this.geodistrictBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.geo_districtTableAdapter = new RussianCityDictionary.russianscitydbDataSetTableAdapters.geo_districtTableAdapter();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.russianscitydbDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.geodistrictBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(26, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(304, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "База данных городов России";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 86);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 100;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(253, 321);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseDoubleClick);
+            // 
+            // russianscitydbDataSet
+            // 
+            this.russianscitydbDataSet.DataSetName = "russianscitydbDataSet";
+            this.russianscitydbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // geodistrictBindingSource
+            // 
+            this.geodistrictBindingSource.DataMember = "geo_district";
+            this.geodistrictBindingSource.DataSource = this.russianscitydbDataSet;
+            // 
+            // geo_districtTableAdapter
+            // 
+            this.geo_districtTableAdapter.ClearBeforeFill = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(286, 86);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.RowHeadersWidth = 100;
+            this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView2.Size = new System.Drawing.Size(297, 321);
+            this.dataGridView2.TabIndex = 2;
+            this.dataGridView2.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView2_CellMouseDoubleClick);
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AllowUserToAddRows = false;
+            this.dataGridView3.AllowUserToDeleteRows = false;
+            this.dataGridView3.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(609, 86);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.ReadOnly = true;
+            this.dataGridView3.RowHeadersVisible = false;
+            this.dataGridView3.RowHeadersWidth = 100;
+            this.dataGridView3.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView3.Size = new System.Drawing.Size(367, 321);
+            this.dataGridView3.TabIndex = 3;
+            this.dataGridView3.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView3_CellMouseDoubleClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(253, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(286, 60);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(297, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(609, 60);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(367, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(986, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1066, 451);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "База данных";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.russianscitydbDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.geodistrictBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private russianscitydbDataSet russianscitydbDataSet;
+        private System.Windows.Forms.BindingSource geodistrictBindingSource;
+        private russianscitydbDataSetTableAdapters.geo_districtTableAdapter geo_districtTableAdapter;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ RussianCityDictionary/Form1.cs
@@ -0,0 +1,125 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RussianCityDictionary
+{
+    public partial class Form1 : Form
+    {
+        DataSet dataSet;
+        MySqlDataAdapter adapter;
+        string connectionstr ="server=localhost;port=3306;username=root;password=somepass1234;database=russianscitydb";
+        MySqlCommandBuilder commandBuilder;
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView2.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView3.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Dbload();
+            //// TODO: данная строка кода позволяет загрузить данные в таблицу "russianscitydbDataSet.geo_district". При необходимости она может быть перемещена или удалена.
+            ////this.geo_districtTableAdapter.Fill(this.russianscitydbDataSet.geo_district);
+            //MySqlCommand command = new MySqlCommand();
+            //command.Connection = db.connector;
+            //command.CommandText = "SELECT * FROM russianscitydb.geo_city;";
+            //MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+            //// Создаем объект Dataset
+            //DataSet ds = new DataSet();
+            //// Заполняем Dataset
+            //adapter.Fill(ds,"Cities");
+            //dataGridView1.DataSource = ds.Tables["Cities"].DefaultView;
+
+            dataGridView3.Columns[0].HeaderText = "Id";
+            dataGridView3.Columns[1].HeaderText = "Id Региона";
+            dataGridView3.Columns[2].HeaderText = "Город";
+
+            //dataGridView1.Columns[0].Visible = false;
+        }
+
+        private void Dbload()
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionstr))
+            {
+                adapter = new MySqlDataAdapter("SELECT * FROM russianscitydb.geo_city;", connection);
+                dataSet = new DataSet();
+                adapter.Fill(dataSet, "russianscitydb.geo_city");
+                dataGridView3.DataSource = dataSet.Tables["russianscitydb.geo_city"];
+                adapter.SelectCommand = new MySqlCommand("SELECT * FROM russianscitydb.geo_district;", connection);
+                adapter.Fill(dataSet, "geo_district");
+                adapter.SelectCommand = new MySqlCommand("SELECT * FROM russianscitydb.geo_regions;", connection);
+                adapter.Fill(dataSet, "geo_regions");
+                dataGridView1.DataSource = dataSet.Tables[1];
+                dataGridView2.DataSource = dataSet.Tables[2];
+            }
+        }
+
+        private void dataGridView1_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionstr))
+            {
+                DataSet ds = new DataSet();
+                MySqlDataAdapter adapter = new MySqlDataAdapter(
+                    $"SELECT * FROM russianscitydb.geo_regions as rg where rg.district_id ={dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString()}", 
+                    connection);
+                adapter.Fill(ds);
+                dataGridView2.DataSource = ds.Tables[0];
+
+            }
+        }
+
+        private void dataGridView2_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionstr))
+            {
+                DataSet ds = new DataSet();
+                MySqlDataAdapter adapter = new MySqlDataAdapter(
+                    $"SELECT * FROM russianscitydb.geo_city as ct where ct.region_id ={dataGridView2.Rows[e.RowIndex].Cells[0].Value.ToString()}",
+                    connection);
+                adapter.Fill(ds);
+                dataGridView3.DataSource = ds.Tables[0];
+
+            }
+        }
+
+        private void dataGridView3_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            Process.Start($"https://www.google.com/maps/place/"+dataGridView3.Rows[e.RowIndex].Cells[2].Value);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            (dataGridView1.DataSource as DataTable).DefaultView.RowFilter =
+        String.Format("name like '{0}%'", textBox1.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dbload();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            (dataGridView2.DataSource as DataTable).DefaultView.RowFilter =
+            String.Format("name like '{0}%'",textBox2.Text);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            (dataGridView3.DataSource as DataTable).DefaultView.RowFilter =
+            String.Format("name like '{0}%'", textBox3.Text);
+        }
+    }
+}
